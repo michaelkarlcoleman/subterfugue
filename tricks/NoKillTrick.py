fdc63393b787b8344e885feba7b5aa9a412cfa5d
Pavel Machek <pavel@sourceforge>
2000-03-25 20:13:29 +0000

    Disable ptrace() -- it is more dangerous than kill (and can not be used inside sandbox, anyway)

--- tricks/NoKillTrick.py
+++ tricks/NoKillTrick.py
@@ -12,7 +12,9 @@ import tricklib
 class NoKill(Trick):
     def usage(self):
         return """
-        Do not let traced processes to kill outside sandbox
+        Do not let traced processes to kill outside sandbox. Do not
+	allow them to use ptrace(), since ptrace() does not work inside
+	sandbox, and could be used to do anything outside sandbox.
 
 	Unless kernel goes _really_ crazy (like an out-of-memory situation)
 	and kills process without telling us (which just should not happen),
@@ -22,10 +24,13 @@ class NoKill(Trick):
     
     def callbefore(self, pid, call, args):
 	tpid = args[0]	# Fixme: make sure he is not shooting at us.
+	if call == 'ptrace':
+	    return (None, -errno.EPERM, None, None)
+
 	assert call == 'kill'
         # print 'Do we have key for ', tpid
         if not tricklib.is_followed(tpid): # Is it inside our sandbox?
             return (None, -errno.EPERM, None, None)
 
     def callmask(self):
-        return { 'kill' : 1 }
+        return { 'kill' : 1, 'ptrace' : 1 }
