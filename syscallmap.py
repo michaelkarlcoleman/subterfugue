df04534eae35500e54d5e9cd253c1db345dfa505
Pavel Machek <pavel@sourceforge>
2002-12-23 11:11:18 +0000

    Support new 2.5 syscalls

--- syscallmap.py
+++ syscallmap.py
@@ -240,6 +240,43 @@ table = (
     ( 3, 0,	"sys_madvise",		"madvise",	None),# 219
     ( 3, 0,	"sys_getdents64",	"getdents64",	None),# 220
     ( 3, 0,	"sys_fcntl64",		"fcntl64",	None),# 221
+    ( 4, 0,	"printargs",		"SYS_222",	None),# 222
+    ( 3, 0,	"sys_security",		"security",	None),# 223
+    ( 0, 0,	"sys_gettid",		"gettid",	None),# 224
+    ( 3, 0,	"sys_readahead",	"readahead",	None),# 225
+    ( 5, TF,	"sys_setxattr",		"setxattr",	None),# 226
+    ( 5, TF,	"sys_setxattr",		"lsetxattr",	None),# 227
+    ( 5, TF,	"sys_fsetxattr",	"fsetxattr",	None),# 228
+    ( 4, TF,	"sys_getxattr",		"getxattr",	None),# 229
+    ( 4, TF,	"sys_getxattr",		"lgetxattr",	None),# 230
+    ( 4, TF,	"sys_fgetxattr",	"fgetxattr",	None),# 231
+    ( 3, TF,	"sys_listxattr",	"listxattr",	None),# 232
+    ( 3, TF,	"sys_listxattr",	"llistxattr",	None),# 233
+    ( 3, TF,	"sys_flistxattr",	"flistxattr",	None),# 234
+    ( 2, TF,	"sys_removexattr",	"removexattr",	None),# 235
+    ( 2, TF,	"sys_removexattr",	"lremovexattr",	None),# 236
+    ( 2, TF,	"sys_fremovexattr",	"fremovexattr",	None),# 237
+    ( 2, TS,	"sys_tkill",		"tkill",	None),# 238
+    ( 4, TF,	"sys_sendfile64",	"sendfile64",	None),# 239
+    ( 4, 0,	"sys_futex",		"futex",	None),# 240
+    ( 3, 0,	"sys_sched_setaffinity","sched_setaffinity",	None),# 241
+    ( 3, 0,	"sys_sched_getaffinity","sched_getaffinity",	None),# 242
+    ( 1, 0,	"sys_set_thread_area",	"set_thread_area",	None),# 243
+    ( 1, 0,	"sys_get_thread_area",	"get_thread_area",	None),# 244
+    ( 2, TI,	"sys_io_setup",		"io_setup",	None),# 245
+    ( 1, TI,	"sys_io_destroy",	"io_destroy",	None),# 246
+    ( 5, TI,	"sys_io_getevents",	"io_getevents",	None),# 247
+    ( 3, TI,	"sys_io_submit",	"io_submit",	None),# 248
+    ( 3, TI,	"sys_io_cancel",	"io_cancel",	None),# 249
+    ( 5, 0,	"sys_alloc_hugepages",	"alloc_hugepages",	None),# 250
+    ( 1, 0,	"sys_free_hugepages",	"free_hugepages",	None),# 251
+    ( 1, 0,	"sys_exit_group",	"exit_group",	None),# 252
+    ( 3, TF,	"sys_lookup_dcookie",	"lookup_dcookie",	None),# 253
+    ( 1, TF,	"sys_epoll_create",	"epoll_create",	None),# 254
+    ( 4, TF,	"sys_epoll_ctl",	"epoll_ctl",	None),# 255
+    ( 4, TF,	"sys_epoll_wait",	"epoll_wait",	None),# 256
+    ( 5, TF,	"sys_remap_file_pages",	"remap_file_pages",	None),# 257
+    ( 2, 0,	"sys_set_tid_address",	"set_tid_address",	None),# 258
     )
 
 
